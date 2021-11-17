f50470f62ffb995ecf4da0c74363d4be3f15ae4a
Danny10ison <pagesngod@gmail.com>
2021-11-17 17:00:18 +0000

    practice

---
+++ jshero/typof.js
@@ -0,0 +1,23 @@
+// to know the type value stored
+
+let y = 3;
+const PI = 3.142;
+let n_ame = "kofi";
+let myName = "Daniel";
+const person = {
+    fname: 'Dan',
+    lname: 'sei'
+};
+let isTrue = true;
+const arr = [
+    "Kofi", "ama",
+    "James", "john"
+];
+
+console.log(y, "is a", typeof(y));
+console.log(PI, "is a", typeof(PI));
+console.log(n_ame, "is a", typeof(n_ame));
+console.log(myName, "is a", typeof(myName));
+console.log(person, "is a", typeof(person));
+console.log(isTrue, "is a", typeof(isTrue));
+console.log(arr, "is a", typeof(arr));
---
+++ jshero/vars.js
@@ -0,0 +1,15 @@
+// Variables in js
+
+// Format
+// let var_name = value;
+let x = 5;
+document.getElementById("vars").innerHTML = x;
+console.log(x);
+// alert(x);
+
+// multiple vars;
+let a = 8,
+    _y = 9,
+    s = 98;
+
+console.log(a, _y, s);
